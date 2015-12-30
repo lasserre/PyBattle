5bd0b6a2b55ff94954c84a8c7a3a37cf6197f026
Caleb Stewart <cstew33@gmail.com>
2015-12-30 12:47:48 -0600

    Added game board logic, and ship logic

--- src/gameLogic/ship.py
+++ src/gameLogic/ship.py
@@ -2,6 +2,7 @@
 # ###################
 
 import shipIds as ids
+import shipOrientations as orient
 
 # ship lengths
 PATROL_BOAT_LEN = 2
@@ -10,13 +11,25 @@ DESTROYER_LEN = 3
 BATTLESHIP_LEN = 4
 CARRIER_LEN = 5
 
+# ship states
+SHIP_ALIVE = 1
+SHIP_SUNK = 0
+
+# ship hole states
+HOLE_HIT = 1
+HOLE_NORMAL = 0
+
 # base ship class
 class ship(object):
 
 	def __init__(self, shipId, length):
 		self._id = shipId
 		self._length = length
+		self.orientation = orient.NORTH
+		self.state = SHIP_ALIVE
+		self.holes = [HOLE_NORMAL] * length
 
+	# the length of the ship
 	@property
 	def length(self):
 	    return self._length
@@ -31,17 +44,73 @@ class ship(object):
 	def yPos(self):
 	    return self._yPos
 
-	# the orientation of the ship (N, S, E, or W)
-	@property
-	def orientation(self):
-	    return self._orientation
+	def isHorizontal(self):
+		return self.orientation == orient.EAST || self.orientation == orient.WEST
 
-	# the setter for the orientation
-	@property
-	def orientation(self, orient):
-		self._orientation = orient
+	def isVertical(self):
+		return self.orientation == orient.NORTH || self.orientation == orient.SOUTH
+
+	# gets the index of the hole at position 'pos'
+	def getHoleIdx(self, pos):
+		
+
+    # returns a list of positions (xPos, yPos)
+    # of each of the holes for this ship
+	def getHoleCoords(self):
+
+		xStart = 0
+		yStart = 0
+
+		if isHorizontal():
+			# take y as-is
+			yStart = self.yPos
+
+			# take leftmost position for x
+			if self.orientation == orient.EAST:
+				xStart = self.xPos
+			else:
+				xStart = self.xPos - (self.length - 1)
+
+		else:
+			# take x as-is
+			xStart = self.xPos
+
+			# take bottom position for y
+			if self.orientation == orient.NORTH:
+				yStart = self.yPos
+			else:
+				yStart = self.yPos - (self.length - 1)
+
+		# return a list of tuples representing the positions of the holes
+		holeCoords = []
+
+		if isHorizontal():
+			# iterate over x positions...
+			for x in range(xStart, xStart + self.length):
+				holeCoords.append((x, yPos))
+		else:
+			# iterate over y positions...
+			for y in range(yStart, yStart + self.length):
+				holeCoords.append((xPos, y))
+
+		return holeCoords
 
 class patrolBoat(ship):
-
 	def __init__(self):
 		super(patrolBoat, self).__init__(ids.PATROL_BOAT, PATROL_BOAT_LEN)
+
+class submarine(ship):
+	def __init__(self):
+		super(submarine, self).__init__(ids.SUBMARINE, SUBMARINE_LEN)
+
+class destroyer(ship):
+	def __init__(self):
+		super(destroyer, self).__init__(ids.DESTROYER, DESTROYER_LEN)
+
+class battleship(ship):
+	def __init__(self):
+		super(battleship, self).__init__(ids.BATTLESHIP, BATTLESHIP_LEN)
+
+class carrier(ship):
+	def __init__(self):
+		super(carrier, self).__init__(ids.CARRIER, CARRIER_LEN)
